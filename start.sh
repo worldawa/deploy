4bb59e692e210a6dff90c389b9a11d7b3cde38c2
worldawa <76438861+worldawa@users.noreply.github.com>
2021-08-09 00:56:33 +0800

    Delete start.sh

--- start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-sudo apt-get install git build-essential cmake automake libtool autoconf
-git clone https://github.com/xmrig/xmrig.git
-mkdir xmrig/build && cd xmrig/scripts
-./build_deps.sh && cd ../build
-cmake .. -DXMRIG_DEPS=scripts/deps
-make -j$(nproc)
