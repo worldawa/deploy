7aea8a33e5c4401b00575d7273bc3f67599fb869
worldawa <76438861+worldawa@users.noreply.github.com>
2021-10-12 21:04:57 +0800

    Update xmrig-termux-start.sh

--- xmrig-termux-start.sh
+++ xmrig-termux-start.sh
@@ -1,9 +1,9 @@
-if [ -d "$xmrig" ]; then
+if [ -e "$xmrig" ]; then
   cd xmrig/build
   ./xmrig -o pool.supportxmr.com:5555 -u 47h5anyu46k6CFWCC9AAqP24SWBS8LcuPdw1L77guFKRTF9WcxNGTs26RWgKagEHxkiPVrjphy7EXPTSe6qd3kpyNNgRL8p
 else
   sed -i 's@^\(deb.*stable main\)$@#\1\ndeb https://mirrors.tuna.tsinghua.edu.cn/termux/termux-packages-24 stable main@' $PREFIX/etc/apt/sources.list
-    sed -i 's@^\(deb.*games stable\)$@#\1\ndeb https://mirrors.tuna.tsinghua.edu.cn/termux/game-packages-24 games stable@' $PREFIX/etc/apt/sources.list.d/game.list
+  sed -i 's@^\(deb.*games stable\)$@#\1\ndeb https://mirrors.tuna.tsinghua.edu.cn/termux/game-packages-24 games stable@' $PREFIX/etc/apt/sources.list.d/game.list
   sed -i 's@^\(deb.*science stable\)$@#\1\ndeb https://mirrors.tuna.tsinghua.edu.cn/termux/science-packages-24 science stable@' $PREFIX/etc/apt/sources.list.d/science.list
   pkg install automake clang git vim cmake -y
   git clone https://ghproxy.com/https://github.com/xmrig/xmrig
